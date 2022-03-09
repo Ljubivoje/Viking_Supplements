a60a484ae7c0ccf2b61b47c4c1bad3a2bfea4d7e
Ljubivoje <95906282+Ljubivoje@users.noreply.github.com>
2022-03-09 16:52:23 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ window.onscroll = ()=>{
 }
 window.onload=function(){
     let url = window.location.pathname;
-    if(url == "/" || url == "ljubivoje.github.io/Viking_Supplements/"){
+    if(url == "Viking_Supplements/" || url == "ljubivoje.github.io/Viking_Supplements/"){
         numberOfArticles()
         ajaxCallBack("assets/data/articles.json",function(result){
             ipisItems(result)  
